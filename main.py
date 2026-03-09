2941b2b8679c002e0da03e3b134b04fecd8f03f1
BeeSpark <dewaldjacobs852@gmail.com>
2026-03-09 11:07:35 +0200

    C: player and player hitbox added, main.py error fixes, circleshape.py added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -10,7 +11,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-
+    dt = 0
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -18,9 +21,9 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
-
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
