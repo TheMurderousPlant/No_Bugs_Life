a2ce9c1492dcee7dc17bafa6f581cb7af7b0ee7f
BeeSpark <dewaldjacobs852@gmail.com>
2026-03-30 12:33:45 +0200

    asteroids now 'breake' when shot

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,8 @@
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 import pygame
 from circleshape import CircleShape
-
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +13,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius > ASTEROID_MIN_RADIUS:
+            log_event("asteroid_split")
+            deviation = random.uniform(20, 50)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            unit_vector = self.velocity
+
+            for i in range(0, 2):
+                asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+                rotated_vector = unit_vector.rotate(deviation)
+                rotated_with_speed_vector = rotated_vector * 1.2
+                asteroid.velocity = rotated_with_speed_vector
+                deviation *= -1
+
